70058e63f5a58b2a33df21f6c98e13b57177f350
Arnaud <arnaudlyon38@gmail.com>
2019-02-01 15:14:52 +0100

    deplacement avion

--- modelObj.js
+++ modelObj.js
@@ -100,12 +100,13 @@ Model.prototype.initParameters = function() {
     this.modelMatrix = mat4.identity();
     this.viewMatrix = mat4.identity();
     this.projMatrix = mat4.identity();
+    this.setPosition(0,0);
 
     // trouver les model/view/proj matrices pour voir l'objet comme vous le souhaitez
 	this.modelMatrix = mat4.scale(this.modelMatrix, [0.1,0.1,0.1]);
 	//this.viewMatrix = lookAt(eye,center,up,dest);
 	this.viewMatrix = mat4.lookAt([0,4,0], [0,0,0], [-1,0,0]);
-	this.projMatrix = mat4.perspective(45.0,1,0.1,30)
+	this.projMatrix = mat4.perspective(45.0,1,0.1,30);
 }
 
 Model.prototype.setParameters = function(elapsed) {
@@ -115,6 +116,9 @@ Model.prototype.setParameters = function(elapsed) {
 Model.prototype.move = function(x,y) {
     // faire bouger votre vaisseau ici
     // --> modifier currentTransform pour ca
+	this.viewMatrix = mat4.lookAt([0,4,0], [this.position[0]+x,0,this.position[1]+y], [-1,0,0]);
+	// this.setPosition(x,y);
+	this.setPosition(this.position[0]+x, this.position[1]+y);
 
 }
 
