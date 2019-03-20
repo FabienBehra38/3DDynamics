4d6a57cfc077942fa8c1cb11bd22ce2c53bcf12e
Tevai <tevaiheiariinuiitepaotahi@hotmail.fr>
2019-03-20 14:39:14 +0100

    refacto setParameters splat

--- splat.js
+++ splat.js
@@ -2,11 +2,11 @@ var splatShader;
 var model;
 
 function initSplatShader() {
-    splatShader = initShaders("splat-vs","splat-fs");
-    
+    splatShader = initShaders("splat-vs", "splat-fs");
+
     // active ce shader
     gl.useProgram(splatShader);
-    
+
     // adresse de la texture uHeightfield dans le shader
     splatShader.positionUniform = gl.getUniformLocation(splatShader, "uPosition");
     splatShader.texUniform = gl.getUniformLocation(splatShader, "uTex");
@@ -14,33 +14,34 @@ function initSplatShader() {
     console.log("splat shader initialized");
 }
 
-function Splat(textureFileName) {
+function Splat(textureFileName, deplacement = 'haut') {
+    this.deplacement = deplacement;
     this.initParameters();
 
-    var wo2 = 0.5*this.width;
-    var ho2 = 0.5*this.height;
+    var wo2 = 0.5 * this.width;
+    var ho2 = 0.5 * this.height;
 
     // un tableau contenant les positions des sommets (sur CPU donc)
     var vertices = [
-	-wo2,-ho2, 0.8,
-	wo2,-ho2, 0.8,
-	wo2, ho2, 0.8,
-	-wo2, ho2, 0.8
+        -wo2, -ho2, 0.8,
+        wo2, -ho2, 0.8,
+        wo2, ho2, 0.8,
+        -wo2, ho2, 0.8
     ];
-    
+
     var coords = [
-	0.0, 0.0, 
-	1.0, 0.0, 
-	1.0, 1.0, 
-	0.0, 1.0
+        0.0, 0.0,
+        1.0, 0.0,
+        1.0, 1.0,
+        0.0, 1.0
     ];
-    
-    var tri = [0,1,2,0,2,3];
-    
-    
+
+    var tri = [0, 1, 2, 0, 2, 3];
+
+
     this.vao = gl.createVertexArray();
     gl.bindVertexArray(this.vao);
-    
+
     // cree un nouveau buffer sur le GPU et l'active
     this.vertexBuffer = gl.createBuffer();
     this.vertexBuffer.itemSize = 3;
@@ -49,7 +50,7 @@ function Splat(textureFileName) {
     gl.enableVertexAttribArray(0);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
     gl.vertexAttribPointer(0, this.vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
-    
+
     // meme principe pour les coords
     this.coordBuffer = gl.createBuffer();
     this.coordBuffer.itemSize = 2;
@@ -68,67 +69,88 @@ function Splat(textureFileName) {
     gl.bindVertexArray(null);
 
     this.loaded = true;
-    
+
     console.log("splat initialized");
 }
 
-Splat.prototype.shader = function() {
+Splat.prototype.shader = function () {
     return splatShader;
 }
 
-Splat.prototype.initParameters = function() {
+Splat.prototype.initParameters = function () {
     this.width = 0.05;
     this.height = 0.05;
-    this.position = [0.0 ,-0.7];
+    this.position = [0.0, -0.7];
 
     this.splatTexture = initTexture("assets/splat.png");
     // we could init some params here 
 }
 
-Splat.prototype.setPosition = function(x,y) {
-	this.position = [x,y];
+Splat.prototype.setPosition = function (x, y) {
+    this.position = [x, y];
 }
 
-Splat.prototype.setParameters = function(elapsed) {
+Splat.prototype.setParameters = function () {
     // we could animate something here
-    this.position[1] += 0.04;
+
+    switch (this.deplacement){
+        case 'haut':
+            this.position[1] += 0.04;
+            break;
+        case 'bas':
+            this.position[1] += -0.04;
+            break;
+        case 'hautD':
+            this.position[1] += 0.04;
+            this.position[0] += 0.01;
+            break;
+        case 'hautG':
+            this.position[1] += 0.04;
+            this.position[0] += -0.01;
+            break;
+        case 'basD':
+            this.position[1] += -0.04;
+            this.position[0] += 0.01;
+            break;
+        case 'basG':
+            this.position[1] += -0.04;
+            this.position[0] += -0.01;
+            break;
+        default:
+            console.warn('deplacement splat unknow');
+    }
+
 }
 
-Splat.prototype.setSpecialParametersRight= function(elapsed) {
+
+Splat.prototype.setSpecialParametersLeft = function (elapsed) {
     // we could animate something here
-    this.position[1] += 0.04;
-    this.position[0] += 0.01;
+
 }
 
-Splat.prototype.setSpecialParametersLeft= function(elapsed) {
-    // we could animate something here
-    this.position[1] += 0.04;
-    this.position[0] -= 0.01;
-}
-
-Splat.prototype.sendUniformVariables = function() {
-    if(this.loaded) {
-        gl.uniform2fv(splatShader.positionUniform,this.position);
+Splat.prototype.sendUniformVariables = function () {
+    if (this.loaded) {
+        gl.uniform2fv(splatShader.positionUniform, this.position);
 
         // how to send a texture:
         gl.activeTexture(gl.TEXTURE0);
-        gl.bindTexture(gl.TEXTURE_2D,this.splatTexture);
+        gl.bindTexture(gl.TEXTURE_2D, this.splatTexture);
         gl.uniform1i(splatShader.texUniform, 0);
     }
 }
 
-Splat.prototype.draw = function() {
-    if(this.loaded) {
-	gl.bindVertexArray(this.vao);
-	gl.drawElements(gl.TRIANGLES, this.triangles.numItems, gl.UNSIGNED_SHORT, 0.5   );
-	gl.bindVertexArray(null);
+Splat.prototype.draw = function () {
+    if (this.loaded) {
+        gl.bindVertexArray(this.vao);
+        gl.drawElements(gl.TRIANGLES, this.triangles.numItems, gl.UNSIGNED_SHORT, 0.5);
+        gl.bindVertexArray(null);
     }
 };
 
-Splat.prototype.collision = function(tabEnnemy){
-    for(var i = 0; i<tabEnnemy.length; i++){
+Splat.prototype.collision = function (tabEnnemy) {
+    for (var i = 0; i < tabEnnemy.length; i++) {
         let pos = tabEnnemy[i].getBBox();
-        if (this.position[0]>pos[0][0] && this.position[0]<pos[1][0] &&  this.position[1]>pos[1][1]&& this.position[1]<pos[0][1]){
+        if (this.position[0] > pos[0][0] && this.position[0] < pos[1][0] && this.position[1] > pos[1][1] && this.position[1] < pos[0][1]) {
             return tabEnnemy[i];
         }
     }
@@ -136,7 +158,7 @@ Splat.prototype.collision = function(tabEnnemy){
 };
 
 
-Splat.prototype.clear = function() {
+Splat.prototype.clear = function () {
     // clear all GPU memory
     gl.deleteBuffer(this.vertexBuffer);
     gl.deleteBuffer(this.coordBuffer);
